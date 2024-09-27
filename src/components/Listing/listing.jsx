8e82010259b5f4b0ce7ba8517a8076f8838de5a7
Tail_Straw <maeva.gr@live.fr>
2024-09-27 23:08:36 +0200

    Add widget listing

---
+++ src/components/Listing/listing.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+
+function Listing({itemList}) {
+    return (
+        <div className='widget-listing'>
+            { itemList.map((item, index) =>
+                <section className='widget-listing-item' key={"listing-item-" + index}>
+                    <h3 className='widget-listing-item-title'>{ item.name }</h3>
+                    <ul className='widget-listing-item-list'>
+                        {
+                            item.list.map((element, index) =>
+                                <li className='widget-listing-item-list-element' key={"listing-item-element-" + index}>{ element }</li>
+                            )
+                        }
+                    </ul>
+                </section>
+            )}
+        </div>
+    )
+}
+
+Listing.Prototypes = {
+    person: PropTypes.object.isRequired
+}
+
+export default Listing;
